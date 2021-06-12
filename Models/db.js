22979a5f40855356df0eb4c416c8c3eb2261ef25
VED <parmarved1@gmail.com>
2021-06-12 11:56:03 +0530

    Update db.js

--- Models/db.js
+++ Models/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 //Connecting to Data Base
-const db_Connect=mongoose.connect('mongodb+srv://VED:g2pS5VgEJvkMZzcF@cluster0.s6iye.mongodb.net/EmployeeDB?retryWrites=true&w=majority',
+const db_Connect=mongoose.connect('mongodb+srv://VED:g2pS5VgEJvkF@cluster0.s6iye.mongodb.net/EmployeeDB?retryWrites=true&w=majority',
     { useUnifiedTopology: true, useNewUrlParser: true })
     .then(res => console.log('Connected to DataBase'))
     .catch(err => console.log(err))
